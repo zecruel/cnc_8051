e8a0c0bd2b5e2392898fb527f7db4b8825131c13
zecruel <zecruel@ig.com.br>
2016-02-04 17:41:24 -0200

    Criação do modulo gui e adequação no modulo principal (modular.py)

---
+++ python/ezeq_maq/gui.py
@@ -0,0 +1,344 @@
+# -*- coding: cp1252 -*-
+import Tkinter as tk
+import threading
+
+import tkFileDialog
+import tkMessageBox
+from math import *		#importa as funcoes matematicas
+import copy
+import re
+import time
+import ezeq_maq.gcode
+import ezeq_maq.render
+from ezeq_maq.bmp import*
+from ezeq_maq.ponto import*
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------- Constroi a GUI  --------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+class Janela(threading.Thread):
+	def __init__(self, group=None, target=None, name=None,
+				args=(), kwargs=None, verbose=None):
+		''' Deriva a classe threading, permitindo a passagem de argumentos
+		na inicializacao da classe '''
+		threading.Thread.__init__(self, group=group, target=target,
+							name=name, verbose=verbose)
+		self.args = args #os argumentos passados sao armaz. em variaveis internas
+		self.kwargs = kwargs #args eh uma tupla e kwargs eh um dicionario
+		
+		#------------- variaveis internas ---------------
+		self.altura = 400 # Tamanho da Área de desenho
+		self.largura = 400
+		self.iter = 0 #iterador
+		self.iter_antigo = -1 #iterador
+		self.iter2 = 0
+		self.continua = 0
+		self.executa = 0
+		
+		self.best_view_x = 0
+		self.best_view_y = 0
+		self.best_view_z = 100
+		
+		self.vel_max = 10
+		self.vel_min = 2000
+		self.movimentos = []
+		
+		self.lista = self.args[0]
+		self.figura = self.args[1]
+		self.codigo = self.args[2]
+
+		#Imagem = bmp._saveBitMapPPM( )
+		self.photo = ''#PhotoImage(data= Imagem)
+		#------------------------------------------------------------
+		
+		# constroi a GUI tk normalmente
+		self.raiz = tk.Tk()
+		self.raiz.protocol("WM_DELETE_WINDOW", self.sai)
+		
+		#self.trava = trava
+		#------------------------------------------
+		#Cria o visualizador de códico e sua barra de rolagem
+		#------------------------------------------
+		self.visual_gcode = tk.Listbox(self.raiz, width=40, height=10, selectmode=tk.SINGLE)
+		self.visual_gcode.grid(row=0, column=0, columnspan=2, rowspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
+		self.vis_barra = tk.Scrollbar(self.raiz)
+		self.vis_barra.grid(row=0, column=2, rowspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
+		self.vis_barra.configure(command=self.visual_gcode.yview)
+		self.visual_gcode.configure(yscrollcommand=self.vis_barra.set)
+		
+		#------------------------------------------
+		#Cria a área de desenho
+		#------------------------------------------
+		self.desenho = tk.Canvas(self.raiz, width=self.largura, height=self.altura, bd=5, relief=tk.RIDGE)
+		self.desenho.grid(row=0, column=3, columnspan=4, rowspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
+		self.desenho.after(50, self.temporal)
+		
+		#------------------------------------------
+		#Cria os comandos de rotação do desenho
+		#------------------------------------------
+		tk.Label(self.raiz, text='Rotacao X:').grid(row=2, column=3, sticky=tk.E)
+		self.rot_x = tk.Scale(self.raiz, orient=tk.HORIZONTAL, resolution=1.0, to=360.0, command=self.roda_x)
+		self.rot_x.grid(row=2, column=4, sticky=tk.W+tk.E+tk.N+tk.S)
+		self.rot_x.set(60)
+		
+		tk.Label(self.raiz, text='Rotacao Y:').grid(row=3, column=3, sticky=tk.E)
+		self.rot_y = tk.Scale(self.raiz, orient=tk.HORIZONTAL, resolution=1.0, to=360.0, command=self.roda_y)
+		self.rot_y.grid(row=3, column=4, sticky=tk.W+tk.E+tk.N+tk.S)
+		
+		tk.Label(self.raiz, text='Rotacao Z:').grid(row=4, column=3, sticky=tk.E)
+		self.rot_z = tk.Scale(self.raiz, orient=tk.HORIZONTAL, resolution=1.0, to=360.0, command=self.roda_z)
+		self.rot_z.grid(row=4, column=4, sticky=tk.W+tk.E+tk.N+tk.S)
+		#self.rot_z.set(30)
+		
+		#------------------------------------------
+		#Cria os comandos de translação do desenho
+		#------------------------------------------
+		tk.Label(self.raiz, text='Pos. X:').grid(row=2, column=5, sticky=tk.E)
+		self.trans_x = tk.Scale(self.raiz, from_=-20., to=20., orient=tk.HORIZONTAL, command=self.olho_x)
+		self.trans_x.grid(row=2, column=6)
+		
+		tk.Label(self.raiz, text='Pos. Y:').grid(row=3, column=5, sticky=tk.E)
+		self.trans_y = tk.Scale(self.raiz, from_=-20., to=20., orient=tk.HORIZONTAL, command=self.olho_y)
+		self.trans_y.grid(row=3, column=6)
+		
+		tk.Label(self.raiz, text='Pos. Z:').grid(row=4, column=5, sticky=tk.E)
+		self.trans_z = tk.Scale(self.raiz, from_=-20., to=20., orient=tk.HORIZONTAL, command=self.olho_z)
+		self.trans_z.grid(row=4, column=6)
+		
+		#------------------------------------------
+		#Cria os botões
+		#------------------------------------------
+		self.b_simula = tk.Button(self.raiz, text='Simula GCODE', command=self.simula)
+		self.b_simula.grid(row=2, column=1)
+		
+		self.b_pausa = tk.Button(self.raiz, text='Pausa', command=self.pausa)
+		self.b_pausa.grid(row=4, column=1)
+		
+		self.b_exec = tk.Button(self.raiz, text='Executa', command=self.exc)
+		self.b_exec.grid(row=3, column=1)
+		
+		#------------------------------------------
+		#Cria o menu da janela
+		#------------------------------------------
+		self.menu = tk.Menu(self.raiz)
+		self.raiz.config(menu=self.menu)
+		self.arq_menu = tk.Menu(self.menu)
+		self.menu.add_cascade(label="Arquivo", menu=self.arq_menu)
+		self.arq_menu.add_command(label="Abrir...", command=self.abrir_arq)
+		self.arq_menu.add_command(label="Fecha", command=self.fechar_arq)
+		self.arq_menu.add_separator()
+		self.arq_menu.add_command(label="Sair", command=self.raiz.destroy)
+		self.aj_menu = tk.Menu(self.menu)
+		self.menu.add_cascade(label="Ajuda", menu=self.aj_menu)
+		self.aj_menu.add_command(label="Sobre...", command=self.sobre)
+		
+		self.e = tk.Entry(self.raiz)
+		self.e.grid(row=4, column=0)
+		self.e.delete(0, tk.END)
+		self.e.insert(0, "a default value")
+	
+	def sai(self):
+		self.raiz.quit()
+
+	def run(self):
+		''' Modifica o metodo run da classe threading'''
+		self.raiz.mainloop()	#entra no mainloop da Tk
+	
+	# -----------------------------------------
+	#Comando de abrir arquivo
+	#------------------------------------------
+	def abrir_arq(self):
+		arquivo = tkFileDialog.askopenfilename(title='Abrir')
+		if arquivo:
+			self.limpa()
+			self.visual_gcode.delete(0,tk.END)
+			with open(arquivo) as f:
+				self.movimentos = []
+				for num_linha, line in enumerate(f):
+					line = line.replace('\r', ' ') #retira os separadores de linha e os substitui por espaços
+					line = line.replace('\n', ' ')
+					self.visual_gcode.insert(tk.END,line)
+					#---------interpreta previamente
+					#--------- para determinar os limites e ajuste da tela
+					self.codigo.linha = line
+					self.codigo.interpreta()
+					
+					#--------------------------------teste
+					
+					for i in range(len(self.codigo.lista)):			#cada objeto da lista interpretada eh adicionado ao desenho
+						pt1 = self.codigo.lista[i].pt1
+						pt2 = self.codigo.lista[i].pt2
+						if ((pt2.x-pt1.x)!=0) or ((pt2.y-pt1.y)!=0) or ((pt2.z-pt1.z)!=0):
+							#movimentos.append((pt2.x-pt1.x, pt2.y-pt1.y, pt2.z-pt1.z, codigo.lista[i].vel, num_linha))
+							self.movimentos.append({'pt1':pt1, 'pt2':pt2, 'vel':self.codigo.lista[i].vel, 'linha':num_linha})
+							self.lista.add_lin(pt1,pt2,self.codigo.lista[i].vel)
+					#--------------------------------------------------------------------------------------------------------------
+			self.best_view_z = (1.3*(max(self.codigo.x_max, self.codigo.y_max, self.codigo.z_max) - min(self.codigo.x_min, self.codigo.y_min, self.codigo.z_min)))
+			self.trans_z.set(0)
+		
+			self.best_view_x = ((self.codigo.x_max - self.codigo.x_min)/2) + self.codigo.x_min
+			self.trans_x.set(0)
+		
+			self.best_view_y = ((self.codigo.y_max - self.codigo.y_min)/2) + self.codigo.y_min
+			self.trans_y.set(0)
+			
+			self.lista.olho_x = self.best_view_x
+			self.lista.olho_y = self.best_view_y
+			if self.best_view_z > 0:
+				self.lista.olho_z = self.best_view_z
+			
+			self.lista.rot_x = int(self.rot_x.get())
+			self.lista.rot_y = int(self.rot_y.get())
+			self.lista.rot_z = int(self.rot_z.get())
+
+			if self.codigo.f_max:
+				self.lista.vel_max = 2 * self.codigo.f_max
+				self.lista.vel_min = self.codigo.f_min
+			else:
+				self.lista.vel_max = self.lista.vel_min = 1.0
+			#print teste
+			self.codigo.limpa()
+			self.redesenha()
+	
+	#------------------------------------------
+	#Comando de fechar arquivo
+	#--------------------------------------------
+	def fechar_arq(self):
+		if tkMessageBox.askokcancel("Fechar", "Deseja fechar o arquivo?"):
+			self.visual_gcode.delete(0,tk.END)
+	
+	#------------------------------------------
+	#Informações do programa
+	#------------------------------------------
+	def sobre(self):
+		label = tkMessageBox.showinfo("Sobre o programa", "Interpretador, simulador de Gcode \n\n Copyright \n BeerWare")
+	
+	
+	#------------------------------------------
+	#rotina temporal
+	#--------------------------------------------
+	def temporal(self):
+		#global contador
+		#global exec_pronto
+		#global movimentos
+		
+		if  self.continua & (self.iter < len(self.movimentos)):
+			self.visual_gcode.selection_clear(0,tk.END)
+			self.visual_gcode.selection_set(self.movimentos[self.iter]['linha'])
+			if (self.movimentos[self.iter]['linha']%23 == 0): 
+				self.visual_gcode.yview(self.movimentos[self.iter]['linha'])
+			#codigo.linha = self.visual_gcode.get(self.iter)
+			#codigo.interpreta()
+			
+			if self.executa:
+				if len(self.movimentos) > 0:
+					pt1 = self.movimentos[self.iter]['pt1']
+					pt2 = self.movimentos[self.iter]['pt2']
+					#vel_min = 10.0
+					
+					#teste
+					if ((pt2.x-pt1.x)!=0) or ((pt2.y-pt1.y)!=0) or ((pt2.z-pt1.z)!=0):
+						dados = converte_maq(pt2.x-pt1.x,pt2.y-pt1.y,pt2.z-pt1.z,self.movimentos[self.iter]['vel'])
+						
+					
+					cor = rgb(self.movimentos[self.iter]['vel'],vel_min, vel_max)
+					#tela.lista_orig.append(linha(pt1, pt2, cor))
+
+					#tela.transforma()
+					self.redesenha()
+					
+					self.iter = self.iter + 1
+				
+		#self.e.delete(0, tk.END) #teste
+		#self.e.insert(0, contador) #teste
+		
+		self.desenho.after(10, self.temporal) # reagenda
+	
+	
+	#------------------------------------------
+	#Comando de simular GCODE
+	#--------------------------------------------
+	def simula(self):
+		self.limpa()
+		self.continua = 1
+		
+	def pausa(self):
+		self.continua = 0
+		self.trava.acquire() #teste
+			
+	def exc(self):
+		self.limpa()
+		self.executa=1
+		self.continua = 1
+		tarefa = threading.Thread(target=background, args = (parar,trava))
+		tarefa.start()
+		
+	def limpa(self):
+		self.desenho.delete("all")
+		self.continua = 0
+		self.executa=0 
+		self.iter = 0
+		self.iter_antigo = -1
+		self.codigo.limpa()
+		self.lista.limpa()
+		self.redesenha()
+		
+	#-------------------------------------------
+	#Comando de redesenhar a tela
+	#-------------------------------------------	
+	def redesenha(self):
+		self.desenho.delete("all")
+		#bmp = BitMap( self.largura, self.altura, Color.WHITE )
+		#global figura
+		#global lista
+		self.figura.limpa()
+		#print lista.num
+		self.lista.draw(self.figura)
+		Imagem = self.figura.ret_ppm()
+		self.photo = tk.PhotoImage(data= Imagem)
+		self.desenho.create_image(8,8, image=self.photo, anchor=tk.NW)
+	
+	#-------------------------------------------
+	#Comando do slider de rotação x
+	#-------------------------------------------
+	def roda_x(self, valor):
+		self.lista.rot_x = float(valor)
+		self.redesenha()
+
+	#-------------------------------------------
+	#Comando do slider de rotação y
+	#-------------------------------------------
+	def roda_y(self, valor):
+		self.lista.rot_y = float(valor)
+		self.redesenha()
+		
+	#-------------------------------------------
+	#Comando do slider de rotação z
+	#-------------------------------------------
+	def roda_z(self, valor):
+		self.lista.rot_z = float(valor)
+		self.redesenha()
+		
+	#-------------------------------------------
+	#Comando do spinbox de translação x
+	#-------------------------------------------
+	def olho_x(self, valor):		
+		self.lista.olho_x = int(valor)*0.1*self.best_view_x + self.best_view_x
+		self.redesenha()
+		
+		
+	#-------------------------------------------
+	#Comando do spinbox de translação y
+	#-------------------------------------------
+	def olho_y(self, valor):
+		self.lista.olho_y = int(valor)*0.2*self.best_view_y +self.best_view_y
+		self.redesenha()
+
+	#-------------------------------------------
+	#Comando do spinbox de translação z
+	#-------------------------------------------
+	def olho_z(self, valor):
+		a = int(valor)*.2*self.best_view_z +self.best_view_z
+		if a > 0:
+			self.lista.olho_z = a
+		self.redesenha()
--- python/modular.py
+++ python/modular.py
@@ -1,5 +1,5 @@
 # -*- coding: cp1252 -*-
-from Tkinter import *
+#from Tkinter import *
 import tkFileDialog
 import tkMessageBox
 from math import *		#importa as funcoes matematicas
@@ -9,17 +9,9 @@ import time
 import threading
 import ezeq_maq.gcode
 import ezeq_maq.render
-from ezeq_maq.bmp import*
+import ezeq_maq.gui
+#from ezeq_maq.bmp import*
 from ezeq_maq.ponto import*
-
-
-vel_max = 2000
-vel_min = 10.0
-contador = 0
-exec_pronto = 0
-movimentos = []
-figura = ezeq_maq.render.bitmap(400,400,(255,255,255))
-lista = ezeq_maq.render.wireframe()
 
 def background(parar,trava):
 	global contador
@@ -109,333 +101,25 @@ def converte_maq(x=0.0,y=0.0,z=0.0,vel=1.0):
 	
 	return d_maq(tick_t, tick_x, tick_y, tick_z)
 		
-
+vel_max = 2000
+vel_min = 10.0
+contador = 0
+exec_pronto = 0
+movimentos = []
+figura = ezeq_maq.render.bitmap(400,400,(255,255,255))
+lista = ezeq_maq.render.wireframe()
 codigo = ezeq_maq.gcode.Gcode()
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-#--------- Constroi a GUI  --------------------------------------
-#-------------------------------------------------------------------------------------------------------------------------------------------------------
-class Janela:
-	#------------- Tamanho da Área de desenho---------------
-	altura = 400
-	largura = 400
-	#------------------------------------------------------------
-	iter = 0 #iterador
-	iter_antigo = -1 #iterador
-	iter2 = 0
-	continua = 0
-	executa = 0
 	
-	best_view_x = 0
-	best_view_y = 0
-	best_view_z = 100
-
-	#Imagem = bmp._saveBitMapPPM( )
-	photo = ''#PhotoImage(data= Imagem)
-	
-	# -----------------------------------------
-	#Comando de abrir arquivo
-	#------------------------------------------
-	def abrir_arq(self):
-		global vel_max
-		global vel_min
-		global movimentos
-		global lista
-		arquivo = tkFileDialog.askopenfilename(title='Abrir')
-		if arquivo != "":
-			self.limpa()
-			self.visual_gcode.delete(0,END)
-			with open(arquivo) as f:
-				movimentos = []
-				for num_linha, line in enumerate(f):
-					line = line.replace('\r', ' ') #retira os separadores de linha e os substitui por espaços
-					line = line.replace('\n', ' ')
-					self.visual_gcode.insert(END,line)
-					#---------interpreta previamente
-					#--------- para determinar os limites e ajuste da tela
-					codigo.linha = line
-					codigo.interpreta()
-					
-					#--------------------------------teste
-					
-					for i in range(len(codigo.lista)):			#cada objeto da lista interpretada eh adicionado ao desenho
-						pt1 = codigo.lista[i].pt1
-						pt2 = codigo.lista[i].pt2
-						if ((pt2.x-pt1.x)!=0) or ((pt2.y-pt1.y)!=0) or ((pt2.z-pt1.z)!=0):
-							#movimentos.append((pt2.x-pt1.x, pt2.y-pt1.y, pt2.z-pt1.z, codigo.lista[i].vel, num_linha))
-							movimentos.append({'pt1':pt1, 'pt2':pt2, 'vel':codigo.lista[i].vel, 'linha':num_linha})
-							lista.add_lin(pt1,pt2,codigo.lista[i].vel)
-					#--------------------------------------------------------------------------------------------------------------
-			self.best_view_z = (1.3*(max(codigo.x_max, codigo.y_max, codigo.z_max) - min(codigo.x_min, codigo.y_min, codigo.z_min)))
-			self.trans_z.set(0)
-		
-			self.best_view_x = ((codigo.x_max - codigo.x_min)/2) + codigo.x_min
-			self.trans_x.set(0)
-		
-			self.best_view_y = ((codigo.y_max - codigo.y_min)/2) + codigo.y_min
-			self.trans_y.set(0)
-			
-			lista.olho_x = self.best_view_x
-			lista.olho_y = self.best_view_y
-			if self.best_view_z > 0:
-				lista.olho_z = self.best_view_z
-			
-			lista.rot_x = int(self.rot_x.get())
-			lista.rot_y = int(self.rot_y.get())
-			lista.rot_z = int(self.rot_z.get())
-
-			if codigo.f_max:
-				lista.vel_max = 2 * codigo.f_max
-				lista.vel_min = codigo.f_min
-			else:
-				lista.vel_max = lista.vel_min = 1.0
-			#print teste
-			codigo.limpa()
-			self.redesenha()
-	
-	#------------------------------------------
-	#Comando de fechar arquivo
-	#--------------------------------------------
-	def fechar_arq(self):
-		if tkMessageBox.askokcancel("Fechar", "Deseja fechar o arquivo?"):
-			self.visual_gcode.delete(0,END)
-	
-	#------------------------------------------
-	#Informações do programa
-	#------------------------------------------
-	def sobre(self):
-		label = tkMessageBox.showinfo("Sobre o programa", "Interpretador, simulador de Gcode \n\n Copyright \n BeerWare")
-	
-	
-	#------------------------------------------
-	#rotina temporal
-	#--------------------------------------------
-	def temporal(self):
-		global contador
-		global exec_pronto
-		global movimentos
-		
-		if  self.continua & (self.iter < len(movimentos)):
-			self.visual_gcode.selection_clear(0,END)
-			self.visual_gcode.selection_set(movimentos[self.iter]['linha'])
-			if (movimentos[self.iter]['linha']%23 == 0): 
-				self.visual_gcode.yview(movimentos[self.iter]['linha'])
-			#codigo.linha = self.visual_gcode.get(self.iter)
-			#codigo.interpreta()
-			
-			if self.executa:
-				if len(movimentos) > 0:
-					pt1 = movimentos[self.iter]['pt1']
-					pt2 = movimentos[self.iter]['pt2']
-					#vel_min = 10.0
-					
-					#teste
-					if ((pt2.x-pt1.x)!=0) or ((pt2.y-pt1.y)!=0) or ((pt2.z-pt1.z)!=0):
-						dados = converte_maq(pt2.x-pt1.x,pt2.y-pt1.y,pt2.z-pt1.z,movimentos[self.iter]['vel'])
-						
-					
-					cor = rgb(movimentos[self.iter]['vel'],vel_min, vel_max)
-					#tela.lista_orig.append(linha(pt1, pt2, cor))
-
-					#tela.transforma()
-					self.redesenha()
-					
-					self.iter = self.iter + 1
-				
-		self.e.delete(0, END) #teste
-		self.e.insert(0, contador) #teste
-		
-		self.desenho.after(10, self.temporal) # reagenda
-	
-	
-	#------------------------------------------
-	#Comando de simular GCODE
-	#--------------------------------------------
-	def simula(self):
-		self.limpa()
-		self.continua = 1
-		
-	def pausa(self):
-		self.continua = 0
-		self.trava.acquire() #teste
-			
-	def exc(self):
-		self.limpa()
-		self.executa=1
-		self.continua = 1
-		tarefa = threading.Thread(target=background, args = (parar,trava))
-		tarefa.start()
-		
-	def limpa(self):
-		self.desenho.delete("all")
-		self.continua = 0
-		self.executa=0 
-		self.iter = 0
-		self.iter_antigo = -1
-		codigo.limpa()
-		lista.limpa()
-		self.redesenha()
-		
-	#-------------------------------------------
-	#Comando de redesenhar a tela
-	#-------------------------------------------	
-	def redesenha(self):
-		self.desenho.delete("all")
-		#bmp = BitMap( self.largura, self.altura, Color.WHITE )
-		global figura
-		global lista
-		figura.limpa()
-		#print lista.num
-		lista.draw(figura)
-		Imagem = figura.ret_ppm()
-		self.photo = PhotoImage(data= Imagem)
-		self.desenho.create_image(8,8, image=self.photo, anchor=NW)
-	
-	#-------------------------------------------
-	#Comando do slider de rotação x
-	#-------------------------------------------
-	def roda_x(self, valor):
-		lista.rot_x = float(valor)
-		self.redesenha()
-
-	#-------------------------------------------
-	#Comando do slider de rotação y
-	#-------------------------------------------
-	def roda_y(self, valor):
-		lista.rot_y = float(valor)
-		self.redesenha()
-		
-	#-------------------------------------------
-	#Comando do slider de rotação z
-	#-------------------------------------------
-	def roda_z(self, valor):
-		lista.rot_z = float(valor)
-		self.redesenha()
-		
-	#-------------------------------------------
-	#Comando do spinbox de translação x
-	#-------------------------------------------
-	def olho_x(self, valor):		
-		lista.olho_x = int(valor)*0.1*self.best_view_x + self.best_view_x
-		self.redesenha()
-		
-		
-	#-------------------------------------------
-	#Comando do spinbox de translação y
-	#-------------------------------------------
-	def olho_y(self, valor):
-		lista.olho_y = int(valor)*0.2*self.best_view_y +self.best_view_y
-		self.redesenha()
-
-	#-------------------------------------------
-	#Comando do spinbox de translação z
-	#-------------------------------------------
-	def olho_z(self, valor):
-		a = int(valor)*.2*self.best_view_z +self.best_view_z
-		if a > 0:
-			lista.olho_z = a
-		self.redesenha()
-
-	def __init__(self,toplevel,parar, trava):
-		self.trava = trava
-		#------------------------------------------
-		#Cria o visualizador de códico e sua barra de rolagem
-		#------------------------------------------
-		self.visual_gcode = Listbox(toplevel, width=40, height=10, selectmode=SINGLE)
-		self.visual_gcode.grid(row=0, column=0, columnspan=2, rowspan=2, sticky=W+E+N+S)
-		self.vis_barra = Scrollbar(toplevel)
-		self.vis_barra.grid(row=0, column=2, rowspan=2, sticky=W+E+N+S)
-		self.vis_barra.configure(command=self.visual_gcode.yview)
-		self.visual_gcode.configure(yscrollcommand=self.vis_barra.set)
-		
-		#------------------------------------------
-		#Cria a área de desenho
-		#------------------------------------------
-		self.desenho = Canvas(toplevel, width=self.largura, height=self.altura, bd=5, relief=RIDGE)
-		self.desenho.grid(row=0, column=3, columnspan=4, rowspan=2, sticky=W+E+N+S)
-		self.desenho.after(50, self.temporal)
-		
-		#------------------------------------------
-		#Cria os comandos de rotação do desenho
-		#------------------------------------------
-		Label(toplevel, text='Rotacao X:').grid(row=2, column=3, sticky=E)
-		self.rot_x = Scale(toplevel, orient=HORIZONTAL, resolution=1.0, to=360.0, command=self.roda_x)
-		self.rot_x.grid(row=2, column=4, sticky=W+E+N+S)
-		self.rot_x.set(60)
-		
-		Label(toplevel, text='Rotacao Y:').grid(row=3, column=3, sticky=E)
-		self.rot_y = Scale(toplevel, orient=HORIZONTAL, resolution=1.0, to=360.0, command=self.roda_y)
-		self.rot_y.grid(row=3, column=4, sticky=W+E+N+S)
-		
-		Label(toplevel, text='Rotacao Z:').grid(row=4, column=3, sticky=E)
-		self.rot_z = Scale(toplevel, orient=HORIZONTAL, resolution=1.0, to=360.0, command=self.roda_z)
-		self.rot_z.grid(row=4, column=4, sticky=W+E+N+S)
-		#self.rot_z.set(30)
-		
-		#------------------------------------------
-		#Cria os comandos de translação do desenho
-		#------------------------------------------
-		Label(toplevel, text='Pos. X:').grid(row=2, column=5, sticky=E)
-		self.trans_x = Scale(toplevel, from_=-20., to=20., orient=HORIZONTAL, command=self.olho_x)
-		self.trans_x.grid(row=2, column=6)
-		#self.trans_x.delete(0,"end")
-		#self.trans_x.insert(0,-1)
-		#self.trans_x.invoke('buttonup')
-		
-		Label(toplevel, text='Pos. Y:').grid(row=3, column=5, sticky=E)
-		self.trans_y = Scale(toplevel, from_=-20., to=20., orient=HORIZONTAL, command=self.olho_y)
-		self.trans_y.grid(row=3, column=6)
-		#self.trans_y.delete(0,"end")
-		#self.trans_y.insert(0,-1)
-		#self.trans_y.invoke('buttonup')
-		
-		Label(toplevel, text='Pos. Z:').grid(row=4, column=5, sticky=E)
-		self.trans_z = Scale(toplevel, from_=-20., to=20., orient=HORIZONTAL, command=self.olho_z)
-		self.trans_z.grid(row=4, column=6)
-		#self.trans_z.delete(0,"end")
-		#self.trans_z.insert(0,2)
-		#self.trans_z.invoke('buttonup')
-		
-		#------------------------------------------
-		#Cria os botões
-		#------------------------------------------
-		self.b_simula = Button(toplevel, text='Simula GCODE', command=self.simula)
-		self.b_simula.grid(row=2, column=1)
-		
-		self.b_pausa = Button(toplevel, text='Pausa', command=self.pausa)
-		self.b_pausa.grid(row=4, column=1)
-		
-		self.b_exec = Button(toplevel, text='Executa', command=self.exc)
-		self.b_exec.grid(row=3, column=1)
-		
-		#------------------------------------------
-		#Cria o menu da janela
-		#------------------------------------------
-		self.menu = Menu(toplevel)
-		toplevel.config(menu=self.menu)
-		self.arq_menu = Menu(self.menu)
-		self.menu.add_cascade(label="Arquivo", menu=self.arq_menu)
-		self.arq_menu.add_command(label="Abrir...", command=self.abrir_arq)
-		self.arq_menu.add_command(label="Fecha", command=self.fechar_arq)
-		self.arq_menu.add_separator()
-		self.arq_menu.add_command(label="Sair", command=toplevel.destroy)
-		self.aj_menu = Menu(self.menu)
-		self.menu.add_cascade(label="Ajuda", menu=self.aj_menu)
-		self.aj_menu.add_command(label="Sobre...", command=self.sobre)
-		
-		self.e = Entry(toplevel)
-		self.e.grid(row=4, column=0)
-		self.e.delete(0, END)
-		self.e.insert(0, "a default value")
-	
-raiz=Tk()
+janela = ezeq_maq.gui.Janela(args=(lista, figura, codigo))
 
 parar = threading.Event()
 trava = threading.RLock()
 
-Janela(raiz, parar, trava)
+janela.start()
 
-raiz.mainloop()
+#Janela(raiz, parar, trava)
+
+#raiz.mainloop()
 
 parar.set()
 #trava.release()
